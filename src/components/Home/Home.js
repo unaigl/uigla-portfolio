22d8acebc39213cce2ee04412a3349b1029fda8f
unaigl <unaiarki@gmail.com>
2022-07-11 10:09:40 +0200

    home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/home-main.svg";
+import Particle from "../Particle";
+import Resume from "./Resume";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={12} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  🌞
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Unai Igleias</strong>
+              </h1>
+
+              <div
+                className="main-name"
+                style={{ padding: 50, textAlign: "left" }}
+              >
+                <Type />
+              </div>
+            </Col>
+
+            {/* <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col> */}
+          </Row>
+        </Container>
+      </Container>
+      <Resume />
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Home/Resume.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImage from "../../Assets/myImage.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+import "../../style.css";
+
+function Resume() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={3} className="myAvtar">
+            <Tilt className="img-boder-box">
+              <img src={myImage} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+          <Col md={1} />
+          <Col md={8} className="home-about-description">
+            {/* <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="green"> INTRODUCE </span> MYSELF
+            </h1> */}
+            <p className="home-about-body">
+              I had been dealing with several softwares before falling in love
+              with programming
+              <br />
+              <br />I am fluent in
+              <i>
+                <b className="green"> Javascript and Solidity. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="green">DApps and Web Technologies </b> and also in
+                areas related to{" "}
+                <b className="green">ZK roll-ups based products.</b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing new
+              <b className="green"> Smart Contract</b> models and
+              <i>
+                <b className="green">
+                  {" "}
+                  Modern Javascript Library and Frameworks
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="green"> React.js and Threejs</b>
+              </i>
+              <br />
+              <br />
+              I'm interested in Smart Contract{" "}
+              <b className="green"> Security</b>
+              <br />
+              <br />
+              Feel free to <span className="green">connect </span>with me
+            </p>
+          </Col>
+        </Row>
+        {/* <Row>
+          <Col md={12} className="home-about-social">
+            <h3>
+              Feel free to <span className="green">connect </span>with me
+            </h3>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/unaigl"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/Unai_naz"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/unaiiglesias"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/unai_igl/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row> */}
+      </Container>
+    </Container>
+  );
+}
+export default Resume;
