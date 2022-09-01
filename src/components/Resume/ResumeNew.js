3451312172d17a3cfbfa8b0a9b9e489e77787dc8
unaigl <unaiarki@gmail.com>
2022-09-01 17:16:42 +0200

    fixing url

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/CV_Unai Iglesias_ Blockchain Developer.pdf";
+import pdf from "../../Assets/../Assets/CV_UnaiIglesias_BlockchainDeveloper.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+// "https://raw.githubusercontent.com/unaigl/portfolio/master/src/Assets/CV_UnaiIglesias_BlockchainDeveloper.pdf";
 const resumeLink =
-  "https://raw.githubusercontent.com/unaigl/portfolio/master/src/Assets/CV_Unai Iglesias_ Blockchain Developer.pdf";
+  "https://raw.github.com/unaigl/uigla-portfolio/master/src/Assets/CV_UnaiIglesias_BlockchainDeveloper.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
