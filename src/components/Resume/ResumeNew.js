e60615460dc5e4a34e82d626403a888de9949811
unaigl <unaiarki@gmail.com>
2022-09-01 18:17:37 +0200

    CV modified

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,7 +6,7 @@ import pdf from "../../Assets/../Assets/CV_UnaiIglesias_BlockchainDeveloper.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`; // TODO:
 
 // "https://raw.githubusercontent.com/unaigl/portfolio/master/src/Assets/CV_UnaiIglesias_BlockchainDeveloper.pdf";
 const resumeLink =
