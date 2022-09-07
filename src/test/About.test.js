4c22a30735471fd74cc216a5d14126705d2797d1
unaigl <unaiarki@gmail.com>
2022-09-07 13:19:58 +0200

    testing about component

--- src/test/About.test.js
+++ src/test/About.test.js
@@ -5,20 +5,25 @@ import { render, screen } from "@testing-library/react";
 import About from "../components/About/About";
 import { prettyDOM } from "@testing-library/dom";
 
-test("1er test", () => {
-  const view = render(<About />);
+describe("<About />", () => {
+  const call = () => {
+    return render(<About />);
+  };
 
-  screen.getByText("Know Who"); // Con este metodo puede que te quedes con la duda de si te lanzara un throw
-});
-test("2er test", () => {
-  const view = render(<About />);
-  //   screen.debug();
+  test("1er test", () => {
+    call();
 
-  expect(view.container).toHaveTextContent("Know Who");
+    screen.getByText("Know Who"); // Con este metodo puede que te quedes con la duda de si te lanzara un throw
+  });
+  test("2er test", () => {
+    const view = call();
+    //   screen.debug();
+    expect(view.container).toHaveTextContent("Know Who");
+  });
+  test("3er test", () => {
+    const view = call();
+    const strong = view.container.querySelector("strong");
+    console.log(prettyDOM(strong));
+    //   console.log(view);
+  });
 });
-// test("3er test", () => {
-//   const view = render(<About />);
-//   const strong = view.container.querySelector("strong");
-//   console.log(prettyDOM(strong));
-//   //   console.log(view);
-// });
